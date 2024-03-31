cf24d34816ac34b5e5a27f5818b795b40ecbaae5
igordg777 <igordg@mail.ru>
2024-04-01 00:38:00 +0300

    front edite, back treatment

--- app.js
+++ app.js
@@ -18,12 +18,72 @@ app.get('/edite', function (req, res) {
     res.render('edite')
 })
 
-app.get('/serch', function (req, res) {
-    res.render('serch')
+app.get('/search', function (req, res) {
+    res.render('search')
 })
 
 app.get('/users', function (req, res) {
     res.render('users')
 })
+
+
+app.get('/addUser', function (req, res) {
+    console.log(req.query);
+    console.log(req.query.login_tg);
+    console.log(req.query.name_guest);
+
+    let result_str_to_save = `INSERT INTO user_access_list (username, name_guest) VALUES ('${req.query.login_tg}', '${req.query.name_guest}');`
+
+    console.log(result_str_to_save);
+
+    // pool.connect(function (err, client, done) {
+
+    //   if (err) {
+    //     console.log("Can not connect to the DB" + err);
+    //   }
+
+
+    //   client.query(result_str_to_save, function (err, result) {
+    //     done();
+    //     if (err) {
+    //       console.log(err);
+    //       res.status(400).send(err);
+    //     }
+    //     console.log(result.rows);
+
+    //     res.status(200).json({ respone: "Пользователь успешно сохранен" })
+    //   })
+    // })
+});
+
+
+app.get('/disableAccess', function (req, res) {
+    console.log(req.query);
+    console.log(req.query.login_tg);
+    console.log(req.query.name_guest);
+
+    let result_str_to_save = `DELETE FROM user_access_list WHERE username='${req.query.login_tg}';`
+
+    console.log(result_str_to_save);
+
+    // pool.connect(function (err, client, done) {
+
+    //   if (err) {
+    //     console.log("Can not connect to the DB" + err);
+    //   }
+
+
+    //   client.query(result_str_to_save, function (err, result) {
+    //     done();
+    //     if (err) {
+    //       console.log(err);
+    //       res.status(400).send(err);
+    //     }
+    //     console.log(result.rows);
+
+    //     res.status(200).json({ respone: "Доступ для пользователя ограничен" })
+    //   })
+    // })
+});
 
 app.listen(3000, console.log('Server work on port 3000'));
